4ddbcb7d908562ff38b47923adf7bb76516a7d1b
waterrbottle <112860891+waterrbottle@users.noreply.github.com>
2025-03-08 23:30:38 +0100

    posortowanie i spawning przeciwnikow

--- objects/enemy/enemy.gd
+++ objects/enemy/enemy.gd
@@ -1,15 +1,21 @@
 extends Area3D
-var angle = 90
-var speed = 0.05
+var angle = randf_range(0,360)
+var dia = randf_range(15,20)
+var speed = randf_range(0.1,0.5)
+var posy = 0
+var sin = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	posy = position.y
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	position.x += speed*sin(angle)
-	position.z += speed*cos(angle)
-	angle += delta
+	position.x = dia * sin(angle )
+	position.z = dia * cos(angle)
+	angle += delta * speed
+	sin += 1.0
+	position.y = posy + sin(sin/10.0)
 
 #go to x = (distance)sin(angle) and y = (distance)cos(angle)
--- scenes/main.gd
+++ scenes/main.gd
@@ -1,9 +1,12 @@
-extends "res://enemy.gd"
-
+extends Node3D
+var enemy = load("res://objects/enemy/enemy.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	for i in range(50):
+		var inst = enemy.instantiate()
+		inst.position.y = randf_range(1,12)
+		$enemies.add_child(inst)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
